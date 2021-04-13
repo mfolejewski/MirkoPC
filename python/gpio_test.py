2d77325535a0a33f050a7966d06b9ef5a38edd5b
Mirek (adsp531) <58222533+mfolejewski@users.noreply.github.com>
2021-04-13 14:49:20 +0200

    python scripts

---
+++ python/gpio_test.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+from time import *
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
+GPIO.output(16, GPIO.LOW)
+GPIO.output(26, GPIO.LOW)
+GPIO.output(22, GPIO.LOW)
+
+GPIO.output(16, GPIO.HIGH)
+sleep(1)
+GPIO.output(16, GPIO.LOW)
+GPIO.output(26, GPIO.HIGH)
+sleep(1)
+GPIO.output(26, GPIO.LOW)
+GPIO.output(22, GPIO.HIGH)
+sleep(0.1)
+GPIO.output(22, GPIO.LOW)
+
+
